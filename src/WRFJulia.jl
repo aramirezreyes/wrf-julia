428feb84778a511ce7351a21c8da48596d613b55
Argel Ramírez Reyes <aramirezreyes@ucdavis.edu>
2018-03-06 12:13:10 -0800

    Initial commit of this package

--- src/WRFJulia.jl
+++ src/WRFJulia.jl
@@ -1,5 +1,13 @@
 module WRFJulia
-
+using NetCDF
+#Exports
+export getvar
 # package code goes here
+
+
+function getvar(file::AbstractString,varname::AbstractString)
+    return ncread(file,varname);
+end
+
 
 end # module
