5478e56ff9f176f265f4352ce03c3947bb23abf2
Argel Ramírez Reyes <aramirezreyes@ucdavis.edu>
2018-03-06 12:52:55 -0800

    Added two incipient functions to extract and plot

--- src/WRFJulia.jl
+++ src/WRFJulia.jl
@@ -1,13 +1,20 @@
 module WRFJulia
 using NetCDF
+using Plots
 #Exports
 export getvar
 # package code goes here
-
 
 function getvar(file::AbstractString,varname::AbstractString)
     return ncread(file,varname);
 end
 
+function animate_heatmap(varArray,times::Int32,fps=Int32)
+    gr()
+    anim = @animate for i in 1:times
+        p = heatmap(varArray[:,:,1,i])
+    end
+    animate_heatmap=gif(anim,fps=fps);
+end
 
 end # module
